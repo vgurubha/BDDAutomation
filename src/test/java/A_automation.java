a6dbe9b3b5591f2094b23fa703a6671be647a614
bhaskar <bhaskar.velpula05@gmail.com>
2022-11-13 16:03:44 +0530

    uicicygif

---
+++ src/test/java/A_automation.java
@@ -0,0 +1,11 @@
+
+public class A_automation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String a="bhaskar";
+       String b="velpula";
+      System.out.println(b+a);
+	}
+
+}
