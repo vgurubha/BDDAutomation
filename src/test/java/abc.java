2b2163510a1a9b3c12cf80cb37f1096832e0c076
bhaskar <bhaskar.velpula05@gmail.com>
2022-11-16 12:41:55 +0530

    commitmassage

---
+++ src/test/java/abc.java
@@ -0,0 +1,15 @@
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short a=50;
+		byte b=70;
+		int c=20;
+		long d=80;
+		float f=20.5f;
+		double g=50.5;
+		System.out.println(a+b+c+d+f+g);
+	}
+
+}
